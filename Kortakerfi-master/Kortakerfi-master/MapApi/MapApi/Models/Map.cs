5532097a889181284f8355033b00585703aba996
Maddjakni <Maddjakni@gmail.com>
2016-11-22 12:34:24 +0000

    Add files via upload

---
+++ Kortakerfi-master/Kortakerfi-master/MapApi/MapApi/Models/Map.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapApi.Models
+{
+    public class Map
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
